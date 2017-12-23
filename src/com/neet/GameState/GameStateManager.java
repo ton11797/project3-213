6c86cadc4cd5b6d2a9ae9495d05665bdf3f2c77b
Unknown <thirapongpdg@gmail.com>
2017-12-24 01:38:01 +0700

    stage 2

--- src/com/neet/GameState/GameStateManager.java
+++ src/com/neet/GameState/GameStateManager.java
@@ -35,9 +35,9 @@ public class GameStateManager {
 			gameStates[state] = new MenuState(this);
 		else if(state == LEVEL1ASTATE)
 			gameStates[state] = new Level1AState(this);
-		/*else if(state == LEVEL1BSTATE)
+		else if(state == LEVEL1BSTATE)
 			gameStates[state] = new Level1BState(this);
-		else if(state == LEVEL1CSTATE)
+		/*else if(state == LEVEL1CSTATE)
 			gameStates[state] = new Level1CState(this);*/
 	}
 	
