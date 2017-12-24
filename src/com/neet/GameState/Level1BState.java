142a2dd77086ee7ac1fec3077ce96a487c7c53e9
Oat <panuphan1997@gmail.com>
2017-12-24 21:33:37 +0700

    Up3

--- src/com/neet/GameState/Level1BState.java
+++ src/com/neet/GameState/Level1BState.java
@@ -244,12 +244,9 @@ public class Level1BState extends GameState {
 	public void handleInput() {
 		if(Keys.isPressed(Keys.ESCAPE)) gsm.setPaused(true);
 		if(blockInput || player.getHealth() == 0) return;
-		player.setUp(Keys.keyState[Keys.UP]);
 		player.setLeft(Keys.keyState[Keys.LEFT]);
-		player.setDown(Keys.keyState[Keys.DOWN]);
 		player.setRight(Keys.keyState[Keys.RIGHT]);
-		player.setJumping(Keys.keyState[Keys.BUTTON1]);
-		player.setDashing(Keys.keyState[Keys.BUTTON2]);
+		player.setJumping(Keys.keyState[Keys.SPACE]);
 		if(Keys.isPressed(Keys.BUTTON3)) player.setAttacking();
 		if(Keys.isPressed(Keys.BUTTON4)) player.setCharging();
 	}
--- src/com/neet/Handlers/Keys.java
+++ src/com/neet/Handlers/Keys.java
@@ -8,32 +8,25 @@ import java.awt.event.KeyEvent;
 
 public class Keys {
 	
-	public static final int NUM_KEYS = 16;
+	public static final int NUM_KEYS = 7;
 	
 	public static boolean keyState[] = new boolean[NUM_KEYS];
 	public static boolean prevKeyState[] = new boolean[NUM_KEYS];
 	
-	public static int UP = 0;
-	public static int LEFT = 1;
-	public static int DOWN = 2;
-	public static int RIGHT = 3;
-	public static int BUTTON1 = 4;
-	public static int BUTTON2 = 5;
-	public static int BUTTON3 = 6;
-	public static int BUTTON4 = 7;
-	public static int ENTER = 8;
-	public static int ESCAPE = 9;
+	public static int LEFT = 0;
+	public static int RIGHT = 1;
+	public static int SPACE = 2;
+	public static int BUTTON3 = 3;
+	public static int BUTTON4 = 4;
+	public static int ENTER = 5;
+	public static int ESCAPE = 6;
 	
 	public static void keySet(int i, boolean b) {
 		if(i == KeyEvent.VK_LEFT) keyState[LEFT] = b;
-		else if(i == KeyEvent.VK_UP) keyState[UP] = b;
-		else if(i == KeyEvent.VK_DOWN) keyState[DOWN] = b;
 		else if(i == KeyEvent.VK_RIGHT) keyState[RIGHT] = b;
-		else if(i == KeyEvent.VK_SPACE) keyState[BUTTON1] = b;
-		else if(i == KeyEvent.VK_E) keyState[BUTTON2] = b;
+		else if(i == KeyEvent.VK_SPACE) keyState[SPACE] = b;
 		else if(i == KeyEvent.VK_R) keyState[BUTTON3] = b;
 		else if(i == KeyEvent.VK_F) keyState[BUTTON4] = b;
-		else if(i == KeyEvent.VK_ENTER) keyState[ENTER] = b;
 		else if(i == KeyEvent.VK_ESCAPE) keyState[ESCAPE] = b;
 	}
 	
